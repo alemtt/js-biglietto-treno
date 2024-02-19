52431145d946d3e4e52f2ef709788fc4674e8abc
alemtt <ale@mattera.eu>
2024-02-19 16:12:43 +0100

    final commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,16 +4,16 @@ const kmtrip = Number(prompt("insert Km"))
 console.log(userAge, kmtrip)
 
 let ticketprice = (kmtrip * 0.21)
-console.log(ticketprice)
+console.log(ticketprice.toFixed(2))
 
 
 if (userAge < 18){
     let ticketMinor = (ticketprice - (ticketprice/100)*20)
-    console.log(ticketMinor)
+    console.log(ticketMinor.toFixed(2))
 
 } else if  (userAge > 65){
     let ticketElder = (ticketprice - (ticketprice/100)*40)
-    console.log(ticketElder)
+    console.log(ticketElder.toFixed(2))
 } else{
-    console.log(ticketprice)
+    console.log(ticketprice.toFixed(2))
 }
